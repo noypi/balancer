884f29c3c114da1a0eb727a66c5596d4a66deb2e
noypi <adrian.migraso@gmail.com>
2020-08-19 19:49:02 +0800

    fix issue when one router is down

--- run-balancer.ps1
+++ run-balancer.ps1
@@ -4,16 +4,16 @@ $gateway_mac1 = 'd4-40-f0-8a-3a-ca' # globe
 $gateway_mac2 = '8c-4c-ad-06-87-88' # pldt
 $source_interface = 'Ethernet'
 
-function getIPByMAC([string]$mac) {
+function GetIPByMAC([string]$mac) {
     return arp -a | select-string $mac |% { $_.ToString().Trim().Split(" ")[0] }
 }
 
-function getBackgroundProcess() {
+function RunningBalancers() {
     return Get-WmiObject Win32_Process -Filter "Name='powershell.exe' AND CommandLine LIKE '%run-balancer.ps1%'"
 }
 
-function countRunning() {
-    $val = @(getBackgroundProcess).Count
+function CountRunning() {
+    $val = @(RunningBalancers).Count
     if ($val -eq $null) {
         return 0
     } else {
@@ -21,7 +21,7 @@ function countRunning() {
     }
 }
 
-function countRunningForeground() {
+function CountRunningForeground() {
     $val = @(Get-Process | Where-Object { $_.MainWindowTitle -like '*Saif Balancer*' }).Count
     if ($val -eq $null) {
         return 0
@@ -30,7 +30,7 @@ function countRunningForeground() {
     }
 }
 
-function pingViaMac([string]$mac) {
+function PingViaMac([string]$mac) {
     $pingResult = @{
         Lost     = 100
         AvgRtt   = 99999
@@ -56,8 +56,8 @@ function pingViaMac([string]$mac) {
         $maxRttPair, $minRttPair, $avgRttPair = $avgRttLine -split "\|", 3
         $sentPair, $receivedPair, $lostPair = $lostLine -split "\|", 3
 
-        $_, $lost = parseColonProperty($lostPair)
-        $_, $avgRtt = parseColonProperty($avgRttPair)
+        $_, $lost = ParseColonProperty $lostPair
+        $_, $avgRtt = ParseColonProperty $avgRttPair
 
         # example: 0 (0.00%) 
         #    removes (0.00%)
@@ -77,7 +77,7 @@ function pingViaMac([string]$mac) {
     return $resultObj
 }
 
-function parseColonProperty([string]$pair) {
+function ParseColonProperty([string]$pair) {
     # Write-Host "parseColonProperty pair => " $pair
     $key, $value = $pair -split '\:', 2
     $key = $key.Trim()
@@ -86,7 +86,7 @@ function parseColonProperty([string]$pair) {
     return $key, $value
 }
 
-function getGwMetric([string]$gw) {
+function GetGwMetric([string]$gw) {
     if ($gw -eq $null) {
         return 0
     }
@@ -94,25 +94,43 @@ function getGwMetric([string]$gw) {
     return (Get-NetAdapter $source_interface | Get-NetRoute -NextHop $gw).RouteMetric
 }
 
-function ensureGatewayHaveMetric([string]$gw, [int]$metric) {
+function EnsureGatewayHaveMetric([string]$gw, [int]$metric) {
     if ($gw -eq $null -or $gw -eq "") {
         return
     }
-    $currentMetric = getGwMetric($gw)
+    $currentMetric = GetGwMetric $gw
     if ($metric -ne $currentMetric) {
         Write-Host "ensuring " $gw " have metric " $metric
         Get-NetAdapter $source_interface | Set-NetRoute -NextHop $gw -RouteMetric $metric
     }
 }
 
+function SetPrimaryGw([string]$gw, [int]$metric, [int]$nonPrimaryMetric) {
+    if ($gw -eq $null -or $gw -eq "") {
+        return
+    }
+    
+    $current_interface = Get-NetAdapter $source_interface
+    
+    $current_interface | Get-NetRoute | ForEach-Object {
+        if ($_.RouteMetric -lt 20) {
+            if ($_.NextHop -eq $gw) {
+                EnsureGatewayHaveMetric $gw $metric
+            } else {
+                EnsureGatewayHaveMetric $_.NextHop $nonPrimaryMetric
+            }
+        }
+    }
+}
+
 function doWork() {
-    $ipaddress_mac1 = getIPByMAC($gateway_mac1);
-    $ipaddress_mac2 = getIPByMAC($gateway_mac2);
+    $ipaddress_mac1 = GetIPByMAC $gateway_mac1;
+    $ipaddress_mac2 = GetIPByMAC $gateway_mac2;
     Write-Host "IP Address 1 => " $ipaddress_mac1
     Write-Host "IP Address 2 => " $ipaddress_mac2
 
-    $pingResult1 = pingViaMac($gateway_mac1)
-    $pingResult2 = pingViaMac($gateway_mac2)
+    $pingResult1 = PingViaMac $gateway_mac1
+    $pingResult2 = PingViaMac $gateway_mac2
 
     Write-Host "mac1 IP => " $ipaddress_mac1 " , lost => " $pingResult1.Lost ", avgrtt => " $pingResult1.AvgRtt
     Write-Host "mac2 IP => " $ipaddress_mac2 " , lost => " $pingResult2.Lost ", avgrtt => " $pingResult2.AvgRtt
@@ -153,51 +171,32 @@ function doWork() {
     Write-Host "echoResult =>" $echoResult
 
     if ($echoResult -eq $useMac2) {
-        $currentMetric = getGwMetric($ipaddress_mac2)
-        Write-Host "currentMetric => " $currentMetric
-        if (5 -eq $currentMetric) {
-            ensureGatewayHaveMetric $ipaddress_mac1 10
-            Write-Host "already using mac2"
-            return
-        }
-        Write-Host "switching default to mac2 with IP " $ipaddress_mac2
-        ensureGatewayHaveMetric $ipaddress_mac1 10
-        ensureGatewayHaveMetric $ipaddress_mac2 5
-        
+        SetPrimaryGw $ipaddress_mac2 5 10
     } else {
-        $currentMetric = getGwMetric($ipaddress_mac1)
-        Write-Host "currentMetric => " $currentMetric
-        if (5 -eq $currentMetric) {
-            ensureGatewayHaveMetric $ipaddress_mac2 10
-            Write-Host "already using mac1"
-            return
-        }
-        Write-Host "switching default to mac1 with IP " $ipaddress_mac1
-        ensureGatewayHaveMetric $ipaddress_mac1 5
-        ensureGatewayHaveMetric $ipaddress_mac2 10
+        SetPrimaryGw $ipaddress_mac1 5 10
     }
 }
 
 try
 {
-    Write-Host "running => " (countRunning)
-    Write-Host "running foreground => " (countRunningForeground)
+    Write-Host "running => " (CountRunning)
+    Write-Host "running foreground => " (CountRunningForeground)
 
-    if (1 -lt (countRunning) -Or (countRunningForeground) -gt 1) {
+    if (1 -lt (CountRunning) -Or (CountRunningForeground) -gt 1) {
         # enssures only one script is running in the background
         Write-Host "already running"
         return
     }
 
     While ($true) {
-        if ((countRunning) -gt 0 -And (countRunningForeground) -gt 0) {
+        if ((CountRunning) -gt 0 -And (CountRunningForeground) -gt 0) {
             # if foreground and background are running, will stop any of the two
             Write-Host "foreground started running, stopping current process."
-            Write-Host "background Process ID => " (getBackgroundProcess).ProcessID
+            Write-Host "background Process ID => " (RunningBalancers).ProcessID
             return
         }
         
-        if ((countRunning) -gt 1) {
+        if ((CountRunning) -gt 1) {
             # ensures only 1 background is running
             return
         }
